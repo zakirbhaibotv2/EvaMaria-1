10301aef7c68b98b9aa7b8d924203e661b16c955
zakirbhaibotv2 <90923366+zakirbhaibotv2@users.noreply.github.com>
2022-02-28 20:36:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -487,7 +487,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.AUTOFILTER_TXT,
+            text=script.BLAFILTER_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
         )
