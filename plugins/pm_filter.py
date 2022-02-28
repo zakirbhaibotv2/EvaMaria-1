0779d6348715b3cfe8b483565f8464a2176f976b
zakirbhaibotv2 <90923366+zakirbhaibotv2@users.noreply.github.com>
2022-02-28 20:30:42 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -34,7 +34,7 @@ SPELL_CHECK = {}
 async def give_filter(client, message):
     k = await manual_filters(client, message)
     if k == False:
-        await auto_filter(client, message)
+        await bla_filter(client, message)
 
 
 @Client.on_callback_query(filters.regex(r"^next"))
