703088c6838a576bc959f60ecc486e3a10da060d
zakirbhaibotv2 <90923366+zakirbhaibotv2@users.noreply.github.com>
2022-08-11 21:33:19 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -804,33 +804,33 @@ async def manual_filters(client, message, text=False):
                                  await x.delete()
                         else:
                             button = eval(btn)
-                        x = await client.send_message(
+                            x = await client.send_message(
                                 group_id,
                                 reply_text,
                                 disable_web_page_preview=True,
                                 reply_markup=InlineKeyboardMarkup(button),
                                 reply_to_message_id=reply_id
                             )
-                         await asyncio.sleep(10)
-                                 await x.delete()
+                                 await asyncio.sleep(10)
+                                 await x.delete
                     elif btn == "[]":
-                      x=await client.send_cached_media(
+                        x = await client.send_cached_media(
                             group_id,
                             fileid,
                             caption=reply_text or "",
                             reply_to_message_id=reply_id
                         )
-                       await asyncio.sleep(10)
+                                 await asyncio.sleep(10)
                                  await x.delete()
                     else:
                         button = eval(btn)
-                      x=await message.reply_cached_media(
+                        x = await message.reply_cached_media(
                             fileid,
                             caption=reply_text or "",
                             reply_markup=InlineKeyboardMarkup(button),
                             reply_to_message_id=reply_id
                         )
-                       await asyncio.sleep(10)
+                                 await asyncio.sleep(10)
                                  await x.delete()
                 except Exception as e:
                     logger.exception(e)
